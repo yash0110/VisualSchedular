2f81f2a92670467d662b8e1c9448cbef36ac3936
Yash Gandhi <yash_gandhi@intuit.com>
2021-06-27 19:21:44 +0530

    Password basic

--- lib/main.dart
+++ lib/main.dart
@@ -19,25 +19,69 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  // const HomePage({Key? key}) : super(key: key);
+
+  var password="12345678";
+  TextEditingController? textEditingControllerName;
+  String? name;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.calendar_today),
-        title: Text('Visual Schedular'),
-        centerTitle: true,
-        actions: [
-          FloatingActionButton(onPressed: () {
-            Navigator.pushReplacement(context,MaterialPageRoute(builder: (context)=>Schedule()));
-          },
-            child: Icon(Icons.add),)
-        ],
-      ),
-      body: TaskView()
-    );
+        appBar: AppBar(
+          leading: Icon(Icons.calendar_today),
+          title: Text('Visual Schedular'),
+          centerTitle: true,
+          actions: [
+            FloatingActionButton(
+              // onPressed: () {
+              //   Navigator.pushReplacement(
+              //     context,
+              //     MaterialPageRoute(
+              //       builder: (context) => Schedule(),
+              //     ),
+              //   );
+              // },
+              onPressed: () => showDialog<String>(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: const Text('Enter Password'),
+                  content: TextFormField(
+                    controller: textEditingControllerName,
+                    onChanged: (value) => name = value,
+                    decoration: InputDecoration(
+                        // labelText: "",
+                        // counterText: "",
+                        contentPadding:
+                        EdgeInsets.symmetric(horizontal: 10, vertical: 15)),
+                    maxLength: 20,
+                    keyboardType: TextInputType.name,
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, 'Cancel'),
+                      child: const Text('Cancel'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        if (name==password){
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Schedule(),
+                            ),
+                          );
+                        }
+                      },
+                      child: const Text('OK'),
+                    ),
+                  ],
+                ),
+              ),
+              child: Icon(Icons.add),
+            )
+          ],
+        ),
+        body: TaskView());
   }
 }
-
-
