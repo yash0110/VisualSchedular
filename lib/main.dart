867b89e73867c6e7bca73215487cee9dc4617e82
simpu <id.simpu@gmail.com>
2021-06-27 18:10:46 +0530

    TTS half-baked

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:visual_schedular/DisplayTasks.dart';
 import 'package:visual_schedular/task_view.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/task_view.dart
+++ lib/task_view.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:visual_schedular/task_data.dart';
 
@@ -15,6 +15,8 @@ class TaskView extends StatefulWidget {
 class _TaskViewState extends State<TaskView> {
   Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   TaskData? _currentTask;
+  List<TaskData> _taskList = [];
+  FlutterTts flutterTts = FlutterTts();
 
   Future<DateTime?> _getProcessedTime() async {
     final SharedPreferences prefs = await _prefs;
@@ -26,57 +28,16 @@ class _TaskViewState extends State<TaskView> {
     }
   }
 
-  Future<void> _saveProcessedTime() async {
+  Future<void> _saveProcessedTime(TimeData timeData) async {
     final SharedPreferences prefs = await _prefs;
-    TaskData? task = await _getCurrentTask();
-    if (task != null) {
-      DateTime now = DateTime.now();
-      await prefs.setInt('process', DateTime(now.year, now.month, now.day, task.time.hour, task.time.minute).millisecondsSinceEpoch);
-    } else {
-      print('error saving processed time');
-    }
+    DateTime now = DateTime.now();
+    await prefs.setInt(
+        'process',
+        DateTime(now.year, now.month, now.day, timeData.hour, timeData.minute)
+            .millisecondsSinceEpoch);
   }
 
-  // Future<void> _timeoutNotify(TaskData task) async {
-  //   print('timeout ${task.name}, ${task.time.hour}:${task.time.minute}');
-  // }
-  //
-  // Future<List<TaskData>> _getTimeoutTask() async {
-  //   DateTime? processedTime = await _getProcessedTime();
-  //   List<TaskData> list = await loadTaskListAsync();
-  //   DateTime nowDateTime = DateTime.now();
-  //   List<TaskData> result = [];
-  //   TaskData? lastTask;
-  //
-  //   for (final task in list) {
-  //     DateTime endDateTime = DateTime(nowDateTime.year, nowDateTime.month,
-  //         nowDateTime.day, task.time.hour, task.time.minute);
-  //     if (lastTask != null) {
-  //       DateTime startDateTime = DateTime(nowDateTime.year, nowDateTime.month,
-  //           nowDateTime.day, lastTask.time.hour, lastTask.time.minute);
-  //       if (endDateTime.isBefore(nowDateTime)) {
-  //         if (processedTime != null) {
-  //           if (startDateTime.isAfter(processedTime)) {
-  //             result.add(lastTask);
-  //           }
-  //         } else {
-  //           result.add(lastTask);
-  //         }
-  //       }
-  //     }
-  //     lastTask = task;
-  //   }
-  //   return result;
-  // }
-
-  Future<TaskData?> _getCurrentTask() async {
-    // final SharedPreferences prefs = await _prefs;
-    // DateTime now_ = DateTime.now();
-    // await prefs.setInt('process', DateTime(now_.year, now_.month, now_.day).millisecondsSinceEpoch);
-
-
-    List<TaskData> list = await loadTaskListAsync();
-    sortTaskList(list);
+  Future<TaskData?> _getCurrentTask(List<TaskData> list) async {
     TaskData? result;
     DateTime nowDate = _eraseTime(DateTime.now());
     TimeData nowTime = _getTime(DateTime.now());
@@ -89,30 +50,12 @@ class _TaskViewState extends State<TaskView> {
       }
     }
     for (final task in list) {
-      // if (task.time.toMinutes() > minute) {
-      //   break;
-      // }
-      // result = task;
-      // if (task.time.toMinutes() < TimeData(processedTime!.hour, processedTime.minute).toMinutes()) {
-      //   break;
-      // }
       if (task.time.toMinutes() > processedTime.toMinutes()) {
         result = task;
         break;
       }
     }
-    print("${processedTime.hour}:${processedTime.minute}");
-    // DateTime taskDateTime = DateTime(nowDateTime.year, nowDateTime.month,
-    //     nowDateTime.day, result!.time.hour, result.time.minute);
-    // if (processedTime != null) {
-    //   if (processedTime.isBefore(taskDateTime)) {
-    //     return result;
-    //   } else {
-    //     return null;
-    //   }
-    // } else {
-    //   return result;
-    // }
+    print("processedTime ${processedTime.hour}:${processedTime.minute}");
     if (result != null) {
       if (result.time.toMinutes() <= nowTime.toMinutes()) {
         return result;
@@ -120,103 +63,81 @@ class _TaskViewState extends State<TaskView> {
     }
   }
 
-  _longPress() {
-    print('long press');
-    _updateCurrentTask().then((value) {
-      setState(() {
-        _saveProcessedTime();
-      });
-      print('save');
-    }).catchError((err) {
-      print(err);
-    });
+  Future<void> _longPress() async {
+    if (_currentTask != null) {
+      await _saveProcessedTime(_currentTask!.time);
+    }
+    await _updateState();
   }
 
-  Future<void> _updateCurrentTask() async {
-    // List<TaskData> timeout = await _getTimeoutTask();
-    // if (timeout.isNotEmpty) {
-    //   TaskData last = timeout.last;
-    //   for (final task in timeout) {
-    //     await _timeoutNotify(task);
-    //   }
-    //   var nowDateTime = DateTime.now();
-    //   DateTime newProc = DateTime(nowDateTime.year, nowDateTime.month,
-    //       nowDateTime.day, last.time.hour, last.time.minute);
-    //   final SharedPreferences prefs = await _prefs;
-    //
-    //   await prefs.setInt('process', newProc.millisecondsSinceEpoch);
-    // }
+  Future<void> _updateState() async {
+    _taskList = await loadTaskListAsync();
+    if (_taskList.isEmpty) {
+      await _saveProcessedTime(TimeData(0, 0));
+    }
 
-    _getCurrentTask()
-        .then((value) => setState(() {
-              _currentTask = value;
-            }))
-        .catchError((error) {
-      // print(error);
-    });
+    TaskData? task = await _getCurrentTask(_taskList);
+    if (task != _currentTask && mounted) {
+      setState(() {
+        _currentTask = task;
+        print('update current task');
+      });
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    _updateCurrentTask();
-    Timer.periodic(Duration(seconds: 10), (timer) {
-      _updateCurrentTask();
+    _updateState();
+    Timer.periodic(Duration(seconds: 5), (timer) {
+      _updateState();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    _updateCurrentTask().then((value) {
-      setState(() {});
-    });
 
-    Widget child = Center(
-        child: Text(
-      'No Tasks, Good Job!!!',
-      style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
-    ));
     if (_currentTask != null) {
       TaskData task = _currentTask!;
-      child = Column(
-        children: [
-          Center(
-              child: Text(
-            task.name,
-            style:
-                DefaultTextStyle.of(context).style.apply(fontSizeFactor: 3.0),
-          )),
-          Center(
-              child: Text(
-            '${task.time.hour}:${task.time.minute}',
-            style:
-                DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
-          )),
-          Container(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      fit: BoxFit.contain,
-                      image: FileImage(File(
-                          task.path.substring(7, task.path.length - 1)))))),
-          Image.file(File(task.path.substring(7, task.path.length - 1))),
 
-          // Temp Button to set process time
-
-          // TextButton(onPressed: () async {
-          //   DateTime nowDateTime = DateTime.now();
-          //   DateTime endDateTime = DateTime(nowDateTime.year, nowDateTime.month,
-          //       nowDateTime.day, 2, 47);
-          //   final SharedPreferences prefs = await _prefs;
-          //
-          //   await prefs.setInt('process', endDateTime.millisecondsSinceEpoch);
-          // }, child: Text("proc"))
-        ],
+      // Widget when we find a task
+      return GestureDetector(
+        onLongPress: _longPress,
+        child: Column(
+          children: [
+            Center(
+                child: Text(
+              task.name,
+              style:
+                  DefaultTextStyle.of(context).style.apply(fontSizeFactor: 3.0),
+            )),
+            Center(
+                child: Text(
+              '${task.time.hour}:${task.time.minute}',
+              style:
+                  DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
+            )),
+            Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: FileImage(File(
+                            task.path.substring(7, task.path.length - 1)))))),
+            Image.file(File(task.path.substring(7, task.path.length - 1))),
+          ],
+        ),
       );
+    } else {
+
+      // Widget when no task available
+      return Center(
+          child: Text(
+        'No Pending Tasks, Good Job!!!',
+        style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
+      ));
     }
-    return GestureDetector(
-      onLongPress: _longPress,
-      child: child,
-    );
+
+    // Widget
   }
 }
 
